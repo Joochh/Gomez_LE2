167556b1c24b68ff91daebbeb5b0b4b8be6d2f42
Joochh <125891712+Joochh@users.noreply.github.com>
2023-02-28 19:40:22 +0800

    Add files via upload
    Initial Commit (LE2)

---
+++ GomezBlogDB/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserName] NVARCHAR(50) NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NCHAR(10) NOT NULL, 
+    [Password] NCHAR(10) NOT NULL
+)
